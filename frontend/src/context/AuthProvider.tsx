748fe80e3d79eb54495ab27c9a5b588e03d7d3b1
Sara Gallego Méndez <saragm10@hotmail.com>
2026-02-14 02:13:33 +0100

    feat: implement authentication context and provider

---
+++ frontend/src/context/AuthProvider.tsx
@@ -0,0 +1,44 @@
+import { useState, type ReactNode } from 'react';
+import type { LoginCredentials } from '../types';
+import { authService } from '../services/auth.service';
+import { AuthContext } from './AuthContext';
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+    const [token, setToken] = useState<string | null>(() => {
+        return localStorage.getItem('token');
+    });
+
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => {
+        return !!localStorage.getItem('token');
+    });
+
+    const login = async (credentials: LoginCredentials, remember: boolean) => {
+        try {
+            const response = await authService.login(credentials);
+
+            const tokenValue = response.accessToken;
+
+            setToken(tokenValue);
+            setIsAuthenticated(true);
+
+            if (remember) {
+                localStorage.setItem('token', tokenValue);
+            }
+        } catch (error) {
+            console.error("Error en el login:", error);
+            throw error;
+        }
+    };
+
+    const logout = () => {
+        setToken(null);
+        setIsAuthenticated(false);
+        localStorage.removeItem('token');
+    };
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, token, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
